fbb3ac5922d76a29e08d5a06a601f471c59ea1df
guorong <guorong@tuyoogame.com>
2022-03-16 21:52:52 +0800

    asset/source学习

--- 05-asset-modules/src/index.js
+++ 05-asset-modules/src/index.js
@@ -2,6 +2,7 @@ import helloword from './hello-word'
 
 import imgsrc1 from './assets/1.png'
 import imgsrc2 from './assets/duck.svg'
+import exampleTxt from './assets/example.txt'
 helloword()
 
 // asset/resource
@@ -15,3 +16,9 @@ const img2 = document.createElement('img')
 img2.style.cssText="width:400px;height:400px"
 img2.src = imgsrc2
 document.body.appendChild(img2)
+
+// asset/source
+const block = document.createElement('div')
+block.style.cssText="width:200px;height:200px;background:pink"
+block.textContent = exampleTxt
+document.body.appendChild(block)
